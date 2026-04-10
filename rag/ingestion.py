99b5cc0a0cfa4180f899826996b236d222fed640
Jaykape <natthajakntk@gmail.com>
2026-04-10 22:33:29 +0700

    init rag

---
+++ rag/ingestion.py
@@ -0,0 +1,19 @@
+import os
+from langchain_community.document_loaders import TextLoader, DirectoryLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAOEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main():
+    pass
+
+   # 1. Loading the files
+   # 2. Chunking the files
+   # 3. Embedding and Storing in a vector database
+
+
+if __name__ == "__main__":
+    main()
